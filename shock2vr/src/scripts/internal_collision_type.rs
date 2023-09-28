be0fc6e3e7ccb24105a9b11c10e8797d78f1a0af
Tommy Builds <tommybuilds451@gmail.com>
2023-09-28 15:02:02 -0700

    feat: restore collision damage

--- shock2vr/src/scripts/internal_collision_type.rs
+++ shock2vr/src/scripts/internal_collision_type.rs
@@ -3,7 +3,7 @@ use shipyard::{EntityId, Get, View, World};
 
 use crate::physics::PhysicsWorld;
 
-use super::{Effect, MessagePayload, Script};
+use super::{Effect, MessagePayload, Script, Message};
 
 // Script to handle collision type
 pub struct InternalCollisionType {
@@ -37,27 +37,27 @@ impl Script for InternalCollisionType {
         msg: &MessagePayload,
     ) -> Effect {
         match msg {
-            MessagePayload::Collided { with: _ } => {
-                
-
-                // let damage_effect = Effect::Send {
-                //     msg: Message {
-                //         to: *with,
-                //         payload: MessagePayload::Damage { amount: 1.0 },
-                //     },
-                // };
-                //Effect::Multiple(vec![initial_effect, damage_effect])
-                if self.collision_flags.contains(CollisionType::SLAY_ON_IMPACT)
-                {
-                    Effect::SlayEntity { entity_id }
-                } else if self
-                    .collision_flags
-                    .contains(CollisionType::DESTROY_ON_IMPACT)
-                {
-                    Effect::DestroyEntity { entity_id }
-                } else {
-                    Effect::NoEffect
-                }
+            MessagePayload::Collided { with } => {
+                let initial_effect = {
+                    if self.collision_flags.contains(CollisionType::SLAY_ON_IMPACT)
+                    {
+                        Effect::SlayEntity { entity_id }
+                    } else if self
+                        .collision_flags
+                        .contains(CollisionType::DESTROY_ON_IMPACT)
+                    {
+                        Effect::DestroyEntity { entity_id }
+                    } else {
+                        Effect::NoEffect
+                    }
+                };
+                let damage_effect = Effect::Send {
+                    msg: Message {
+                        to: *with,
+                        payload: MessagePayload::Damage { amount: 1.0 },
+                    },
+                };
+                Effect::Multiple(vec![initial_effect, damage_effect])
             }
             _ => Effect::NoEffect,
         }
