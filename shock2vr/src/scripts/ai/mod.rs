d5ce91e5565e47cda4477b5825daf7755e414972
tommybuilds <tommybuilds451@gmail.com>
2023-09-28 17:55:53 -0700

    feat: initial turret AI (#8)
    An initial turret AI that seeks and attacks player
    
    - [x] Add logic for spawning projectile
    - [x] Remove unused `velocity` from CreateEntity effect
    - [x] Add logic for handling GunFlash link
    - [x] Add steering logic that impacts joint
    - [x] Incorporate rotated logic in entity spawn, so that projectile / gunflash spawn in the right spot
    - [x] Restore collision damage
    - [x] Add sound for shooting
    - [x] `is_player_visible`
    - [x] Add expand/contract logic
    - [x] Clean up / remove debug code

--- shock2vr/src/scripts/ai/mod.rs
+++ shock2vr/src/scripts/ai/mod.rs
@@ -4,8 +4,10 @@ pub mod steering;
 mod animated_monster_ai;
 mod behavior;
 mod camera_ai;
+mod turret_ai;
 
 pub use animated_monster_ai::*;
 pub use camera_ai::*;
+pub use turret_ai::*;
 
 use super::{Effect, Message, MessagePayload, Script};
