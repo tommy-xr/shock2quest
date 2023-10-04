0ca80e157dc68b7925d9fff3f50134e6c482fef9
Tommy Builds <tommybuilds451@gmail.com>
2023-10-04 08:28:51 -0700

    experiment: start sketching out vr hand definitions

--- shock2vr/src/vr_config.rs
+++ shock2vr/src/vr_config.rs
@@ -1,17 +1,108 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
+use cgmath::{vec3, Angle, Deg, Quaternion, Rotation3, Vector3};
 use once_cell::sync::Lazy;
 
-static ALLOWED_HAND_MODELS_SET: Lazy<HashSet<&str>> = Lazy::new(|| {
-    let ALLOWED_HAND_MODELS: Vec<&str> = vec!["atek_h", "amp_h", "lasehand"];
+struct VRHandModelPerHandAdjustments {
+    offset: Vector3<f32>,
+    rotation: Quaternion<f32>,
+    scale: Vector3<f32>,
+}
 
-    let mut set = HashSet::new();
-    for model in ALLOWED_HAND_MODELS.iter() {
-        set.insert(*model);
+impl VRHandModelPerHandAdjustments {
+    pub fn new() -> VRHandModelPerHandAdjustments {
+        VRHandModelPerHandAdjustments {
+            offset: Vector3::new(0.0, 0.0, 0.0),
+            rotation: Quaternion::new(0.0, 0.0, 0.0, 1.0),
+            scale: Vector3::new(1.0, 1.0, 1.0),
+        }
     }
-    set
+
+    pub fn rotate_y(self, angle: Deg<f32>) -> VRHandModelPerHandAdjustments {
+        VRHandModelPerHandAdjustments {
+            rotation: self.rotation * Quaternion::from_angle_y(angle),
+            ..self
+        }
+    }
+
+    pub fn flip_x(self) -> VRHandModelPerHandAdjustments {
+        VRHandModelPerHandAdjustments {
+            scale: vec3(-self.scale.x, self.scale.y, self.scale.z),
+            ..self
+        }
+    }
+
+    pub fn flip_z(self) -> VRHandModelPerHandAdjustments {
+        VRHandModelPerHandAdjustments {
+            scale: vec3(self.scale.x, self.scale.y, -self.scale.z),
+            ..self
+        }
+    }
+}
+
+struct VRHandModelAdjustments {
+    left_hand: VRHandModelPerHandAdjustments,
+    right_hand: VRHandModelPerHandAdjustments,
+}
+
+impl VRHandModelAdjustments {
+    pub fn new(
+        left_hand: VRHandModelPerHandAdjustments,
+        right_hand: VRHandModelPerHandAdjustments,
+    ) -> VRHandModelAdjustments {
+        VRHandModelAdjustments {
+            left_hand,
+            right_hand,
+        }
+    }
+}
+
+static HAND_MODEL_POSITIONING: Lazy<HashMap<&str, VRHandModelAdjustments>> = Lazy::new(|| {
+    let mut map = HashMap::new();
+
+    let held_weapon_right = VRHandModelPerHandAdjustments::new().rotate_y(Deg(90.0));
+    let held_weapon_left = held_weapon_right.flip_x();
+
+    // ATEK
+    map.insert(
+        "atek_h",
+        VRHandModelAdjustments::new(held_weapon_left, held_weapon_right),
+    );
+
+    // AMP
+    map.insert(
+        "amp_h",
+        VRHandModelAdjustments::new(held_weapon_left, held_weapon_right),
+    );
+
+    // LASEHAND
+    map.insert(
+        "lasehand",
+        VRHandModelAdjustments::new(held_weapon_left, held_weapon_right),
+    );
+
+    map
 });
 
 pub fn is_allowed_hand_model(model_name: &str) -> bool {
-    ALLOWED_HAND_MODELS_SET.contains(model_name)
+    HAND_MODEL_POSITIONING.contains_key(model_name)
+}
+
+pub fn get_vr_hand_model_adjustments(
+    model_name: &str,
+    is_left_hand: bool,
+) -> VRHandModelPerHandAdjustments {
+    let maybe_adjustments = HAND_MODEL_POSITIONING.get(model_name);
+
+    if maybe_adjustments.is_none() {
+        return VRHandModelPerHandAdjustments::new();
+    }
+
+    let adjustments = maybe_adjustments.unwrap();
+
+    if is_left_hand {
+        adjustments.left_hand
+    } else {
+        adjustments.right_hand
+    }
 }
