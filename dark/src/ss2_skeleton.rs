2d93edbafc0a3515695412f7b88e4470aaa1f79d
Tommy Builds <tommybuilds451@gmail.com>
2025-11-09 09:59:57 -0800

    formatting

--- dark/src/ss2_skeleton.rs
+++ dark/src/ss2_skeleton.rs
@@ -1,9 +1,9 @@
 // ss2_skeleton.rs
 // Helper class to work with skeletons in AI meshes
 
+use log::warn;
 use rpds as immutable;
 use std::collections::HashMap;
-use log::warn;
 
 use cgmath::{Deg, Matrix4, Quaternion, SquareMatrix, Vector3};
 use engine::scene::{SceneObject, VertexPosition, color_material, cube, lines_mesh};
