dd5039407642f13ade0d4b228972c587be052e12
tommy-xr <tommybuilds451@gmail.com>
2026-08-01 03:42:54 -0700

    refactor(dark): merge rectangle importers

--- dark/src/importers/mod.rs
+++ dark/src/importers/mod.rs
@@ -3,25 +3,36 @@ mod audio_importer;
 mod bitmap_animation_importer;
 mod font_importer;
 mod glb_model_importer;
-mod map_position_importer;
 mod model_importer;
 mod motiondb_importer;
+mod rect_list_importer;
 mod skeleton_importer;
 mod song_importer;
 mod strings_importer;
 mod texture_importer;
-mod ui_layout_importer;
 
 pub use animation_clip_importer::*;
 pub use audio_importer::*;
 pub use bitmap_animation_importer::*;
 pub use font_importer::*;
 pub use glb_model_importer::*;
-pub use map_position_importer::*;
 pub use model_importer::*;
 pub use motiondb_importer::*;
+pub use rect_list_importer::*;
 pub use skeleton_importer::*;
 pub use song_importer::*;
 pub use strings_importer::*;
 pub use texture_importer::*;
-pub use ui_layout_importer::*;
+
+#[cfg(test)]
+mod tests {
+    use super::{MAP_POSITION_IMPORTER, UI_LAYOUT_IMPORTER};
+
+    #[test]
+    fn map_and_ui_rects_share_one_importer() {
+        let map_importer = std::ptr::from_ref(&*MAP_POSITION_IMPORTER) as *const ();
+        let ui_importer = std::ptr::from_ref(&*UI_LAYOUT_IMPORTER) as *const ();
+
+        assert_eq!(map_importer, ui_importer);
+    }
+}
